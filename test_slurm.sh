542a271d525664c0a9eaedbfee6cb91ac8ab0bd8
Ghulam Rasool <rasool@rowan.edu>
2019-01-19 23:30:30 -0500

    Update test_slurm.sh

--- test_slurm.sh
+++ test_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p GPU
-#SBATCH --gres=gpu:p100:4
+#SBATCH --gres=gpu:p100:2
 #SBATCH -t 2:00
-#SBATCH --ntasks-per-node 28
+#SBATCH --ntasks-per-node 14
 module load singularity
 singularity exec -nv i-my-tensorflow-gpu-py3.simg python testing-comet/test_mnist.py

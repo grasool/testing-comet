33e3b2bd43d2f34c8f2be427a4652b1caa035ae5
Ghulam Rasool <rasool@rowan.edu>
2019-01-19 23:14:09 -0500

    Delete test_slurm.sh

--- test_slurm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -p GPU-small
-#SBATCH --gres=gpu:p100:2
-#SBATCH -t 5:00
-#SBATCH --ntasks-per-node 28
-module load keras/2.2.0_tf1.7_py3_gpu
-python test_mnist.py

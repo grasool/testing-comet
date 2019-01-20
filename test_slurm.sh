6d5216f11a6c360b2ad84b855e4113e157543227
Ghulam Rasool <rasool@rowan.edu>
2019-01-20 00:09:48 -0500

    Update test_slurm.sh

--- test_slurm.sh
+++ test_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH -p gpu-shared
 #SBATCH --ntasks-per-node 14
-#SBATCH --gres= gpu:p100:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH -t 2:00
 module load singularity
 singularity exec /oasis/scratch/comet/mkandes/temp_project/singularity/images/tensorflow-gpu.img python test_mnist.py

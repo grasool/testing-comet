7f6487da407056e2416e79946d574235dc546163
Ghulam Rasool <rasool@rowan.edu>
2019-01-20 00:02:03 -0500

    Update test_slurm.sh

--- test_slurm.sh
+++ test_slurm.sh
@@ -5,4 +5,4 @@
 #SBATCH -t 2:00
 #SBATCH --ntasks-per-node 24
 module load singularity
-singularity exec /oasis/scratch/comet/mkandes/temp_project/singularity/images/keras-tensorflow-gpu.img python test_mnist.py
+singularity exec /oasis/scratch/comet/mkandes/temp_project/singularity/images/tensorflow-gpu.img python test_mnist.py
